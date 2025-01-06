be09e48557e80a4f42cdfbffcc55d8f15007ff8e
pravallika31792503 <pravallikakasula19@gmail.com>
2025-01-06 10:56:29 +0530

    functions_in_linked_lists

--- data_structures/linked_lists/linked_lists.py
+++ data_structures/linked_lists/linked_lists.py
@@ -15,5 +15,40 @@ class Linkedlist:
             llstr+=str(itr.data)+'-->' if itr.next else str(itr.data)
             itr=itr.next
         print(llstr)
+    def insert_at_begining(self,data):
+        node=Node(data,self.head)
+        self.head=node
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head=Node(data,None)
+            return
+        itr=self.head
+        while itr.next:
+            itr=itr.next
+        itr.next=Node(data,None)
+    def get_length(self):
+        itr=self.head
+        count=0
+        while itr:
+            count+=1
+            itr=itr.next
+        return count
+    def insert_values(self,data_list):
+        self.head=None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    
+
+
+
+if __name__=='__main__':
+    l1=Linkedlist()
+    l1.insert_at_begining(10)
+    l1.insert_at_end(20)
+    l1.insert_values(["banana","mango","grapes","orange"])
+    l1.print()
+    l1.get_length()
+
 
 
